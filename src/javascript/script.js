6db15682f73c9e90652be62d74103697f7e7c3d6
Manuel Quicaia <emanuellquicaia@gmail.com>
2024-05-10 11:11:19 +0100

    primeiro commit

---
+++ src/javascript/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $('#mobile_btn').on('click', function(){
+       $('#mobile_menu').toggleClass('active')
+       $('#mobile_btn').find('i').toggleClass('fa-x')
+    })
+
+    const sections = $('section')
+    const navItems = $('.nav_item')
+
+    $(window).on('scroll', function(){
+        const header = $('header');
+        
+        const scrollPosition = $(window).scrollTop() - header.outerHeight(); 
+        let activeSectinIndex = 0;
+
+        if(scrollPosition <= 0){
+            header.css('box-shadow', 'none')
+        }
+        else{
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)')
+        }
+        sections.each(function(i){
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBotton = sectionTop + section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBotton){
+                activeSectinIndex = i;
+                return false
+            }
+        })
+        navItems.removeClass('active')
+        $(navItems[activeSectinIndex]).addClass('active');
+    });
+    
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+     ScrollReveal().reveal('.dish', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#testimonials_chef', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#feedbacks', {
+        origin: 'right',
+        duration: 2000,
+        distance: '20%'
+    });
+})
